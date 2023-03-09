9e60ed0c7689b42ccfd37bcd2ece29a62450c756
EitoNakahara <rebaria@outlook.jp>
2023-03-09 21:23:39 +0900

    code Done

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ from helpers import apology, login_required
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, Text
-
-import pandas as pd
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import desc
+from db import DATABASE
+import sys
 
 # Configure application
 app = Flask(__name__)
@@ -24,14 +25,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-## db
-user = 'root'
-password = 'eito8110'
-host = 'localhost'
-db_name = 'life_time'
-
-# engineの設定
-engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{db_name}')
+# engineの設定, dbの情報を取得
+engine = create_engine(DATABASE())
 
 # セッションの作成
 db_session = scoped_session(
@@ -50,36 +45,42 @@ Base.query  = db_session.query_property()
 # Baseを継承
 class User(Base):
   
-  # テーブル名
   __tablename__ = 'users'
-  # カラムの定義
-  id = Column(Integer, primary_key=True, autoincrement=True)
-  name = Column(String(100), unique=False)
-  hash = Column(Integer, unique=False)
-  date = Column(Text, unique=False)
+
+  id = Column('id', Integer, primary_key=True, autoincrement=True)
+  username = Column('username', String(200), unique=False)
+  hash = Column('password_hash', String(200), unique=False)
+  date = Column('dateofbirth', TIMESTAMP, unique=False)
   
-  def __init__(self, name=None, hash=None, date=None):
-    self.name = name
+  def __init__(self, id=None, username=None, hash=None, date=None):
+    self.id = id
+    self.username = username
     self.hash = hash
     self.date = date
+
+class Goal(Base):
+  
+  __tablename__ = 'goals'
+
+  goal_id = Column('goal_id', Integer, primary_key=True, autoincrement=True)
+  user_id = Column('user_id', Integer)
+  age = Column('age', Integer, unique=False)
+  goal = Column('goal', String(200), unique=False)
+  detail = Column('detail', String(200))
+  
+  def __init__(self, goal_id=None, user_id=None, age=None, goal=None, detail=None):
+    self.goal_id = goal_id
+    self.user_id = user_id
+    self.age = age
+    self.goal = goal
+    self.detail = detail
     
+def main(args):
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
 
-Base.metadata.create_all(bind=engine)
-
-def read_data():
-  """CSVファイルを読み込み、DBにデータを追加する関数
-  """
-  user_df = pd.read_csv('./data/user.csv')
-
-  for index, _df in user_df.iterrows():
-    row = User(name=_df['name'], hash=_df['hash'], date=_df['date'])
-    # データを追加する
-    db_session.add(row)
-
-  db_session.commit()
-
-read_data()
-
+if __name__ == "__main__":
+    main(sys.argv)
 
 @app.after_request
 def after_request(response):
@@ -90,59 +91,69 @@ def after_request(response):
     return response
 
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    """Show portfolio of stocks"""
-    user_info = db_session.query(User).filter(User.id == session["user_id"]).all()
+    if request.method == 'GET':
+        user_info = db_session.query(User).filter(User.id == session["user_id"]).all()[0]
+        user_date = f'{user_info.date}'
+        user_life = [int(user_date[0:4]), int(user_date[0:4])+80, user_date[5:7], user_date[8:10]]
+        goals = db_session.query(Goal).filter(Goal.user_id == session["user_id"]).order_by(Goal.age).all()
+        return render_template("index.html", user_name=user_info.username, user_life=user_life, goals=goals)
 
-    return render_template("index.html", user_info=user_info[0])
+    else:
+        age = request.form.get('age')
+        goal = request.form.get('goal')
+        detail = request.form.get('detail')
 
+        new_goal = Goal(user_id=session["user_id"], age=age, goal=goal, detail=detail)
+             
+        db_session.add(new_goal)
+        db_session.commit()
+        return redirect('/')
+
+
+@app.route("/create")
+def create():
+        return render_template("create.html")
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Goal.query.get(id)
+
+    db_session.delete(post)
+    db_session.commit()
+    return redirect('/')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
-    # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
-
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
+        rows = db_session.query(User).filter(User.username == request.form.get("username")).all()
 
-        # Query database for username
-        rows = db_session.query(User).filter(User.id == request.form.get("username")).all()
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(rows[0].hash, request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0].id
 
-        # Redirect user to home page
         return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
+    
     else:
-        return render_template("login.html")
+        users = db_session.query(User).all()
+        return render_template("login.html", users=users)
 
 
 @app.route("/logout")
 def logout():
     """Log user out"""
-
-    # Forget any user_id
     session.clear()
-
-    # Redirect user to login form
     return redirect("/login")
 
 
@@ -154,7 +165,7 @@ def register():
         if not username:
             return apology("No username entered!")
 
-        if db_session.query(User).filter(User.name == username).all():
+        if db_session.query(User).filter(User.username == username).all():
             return apology("Sorry! That username already exists!")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
@@ -164,7 +175,7 @@ def register():
             return apology("Please check password!")
 
         user_name = User()
-        user_name.name = username
+        user_name.username = username
         user_name.hash = generate_password_hash(password)
         user_name.date = dateofbirth
         db_session.add(user_name)
